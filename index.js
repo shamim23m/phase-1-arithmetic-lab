a7215fa873b047b40fecfa628aef6d08b0adf2f5
shamim23m <shamimmohamud13@gmail.com>
2024-09-24 12:02:37 +0300

    assingment

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2
+console.log( multiply )
 
-// Write your code here
+var random = Math.floor(Math.random() * 100) + 1;
+console.log(random); 
+
+const num3 = 29
+const num4 = 5
+const mod = num3 % num4
+mod === 4
+console.log( mod )
+
+const max = Math.max(10, 15, 20)
+console.log( max )
+
+
+
+
+
+
+
